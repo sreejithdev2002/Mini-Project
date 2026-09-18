b3a1e5c710d04e6cd2ee07b416840a7f613513b1
Sreejith U <126107363+sreejithdev2002@users.noreply.github.com>
2026-09-18 12:17:47 +0530

    Updated all "assets/" to "Assets/"

--- src/Components/Admin/ProductReviews/ProductReviews.jsx
+++ src/Components/Admin/ProductReviews/ProductReviews.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getAllReviews } from "../../../Services/AdminApi";
-import SampleImg from "../../../assets/Images/example1.webp";
+import SampleImg from "../../../Assets/Images/example1.webp";
 import StarRating from "../../User/StarRating/StarRating";
 import "./ProductReviews.css";
 
